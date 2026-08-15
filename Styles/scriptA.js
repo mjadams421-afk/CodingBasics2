2edcd078b24f0f10bb9e2ceab57e9f08ce7eaa70
Mat <mjadams421@gmail.com>
2026-08-15 13:33:35 +0000

    Added to JS

--- Styles/scriptA.js
+++ Styles/scriptA.js
@@ -9,9 +9,17 @@ const response = document.getElementById('RespA');
   let M = Date.getMonth() + 1;
   let D = Date.getDate();
   let line = "";
+  let hScore = 0;
+  let cScore = 0;
 function getResp() {
   let name = window.prompt("What is your name?");
   let age = window.prompt("What is your age?");
+  let user = window.prompt("pick a number between 1 and 10");
+  let comp = Math.floor(Math.random()*10)+1;
+  let targ = Math.floor(Math.random()*10)+1;
+  if(user === targ){
+    hScore++;
+  }
   line += "Hello" + " " + name + " , " + "as of " + M + "/" + D + "/" + Y + " you are " + age + " years old.";
   response.innerHTML = line;
 }
