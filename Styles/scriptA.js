a89638c935c5876481ecf2c0b96a79c518cb1a69
Mat <mjadams421@gmail.com>
2026-08-16 20:26:40 +0000

    Added score game to JS

--- Styles/scriptA.js
+++ Styles/scriptA.js
@@ -1,7 +1,9 @@
 //Variables from html DOM
 const activateA = document.getElementById('activateA');
 const RespA = document.getElementById('RespA');
-RespA.innerHTML = "test";
+RespA.innerHTML = "Response here";
+const RespB = document.getElementById('RespB');
+RespB.innerHTML = "Score here";
 
 //function
 
@@ -12,6 +14,7 @@ RespA.innerHTML = "test";
   let line = "";
   let hScore = 0;
   let cScore = 0;
+  let linB = "";
 function getResp() {
   let name = window.prompt("What is your name?");
   let age = window.prompt("What is your age?");
@@ -20,6 +23,20 @@ function getResp() {
   let targ = Math.floor(Math.random()*10)+1;
   line += "Hello" + " " + name + " , " + "as of " + M + "/" + D + "/" + Y + " you are " + age + " years old.";
   RespA.innerHTML = line;
-}
+  }
+  if(user === targ) {
+    hScore++;
+  } else if(user !== targ) {
+    hScore += 0;
+  } else if(comp === targ) {
+    cScore++;
+  } else if(comp !== targ) {
+    cScore += 0;
+  } else {
+    hScore += 0;
+    cScore += 0;
+  }
+  lineB += "Your score is " + hScore + " and the computer's score is " + cScore + ".";
+  RespB.innerHTML = lineB;
 
 activateA.addEventListener('click', getResp);
