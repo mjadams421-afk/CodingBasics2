e02a7f54fc6363967b28636a2895686d5de26fbf
Mat <mjadams421@gmail.com>
2026-08-17 22:18:25 +0000

    Added to JS

--- Styles/scriptA.js
+++ Styles/scriptA.js
@@ -21,16 +21,22 @@ function getResp() {
   const age = window.prompt("What is your age?");
   const user = Number(window.prompt("pick a number between 1 and 10"));
   const comp = Math.floor(Math.random() * 10) + 1;
-  const targ = Math.floor(Math.random() * 10) + 1;
+  const targ = Math.ceil(Math.random() * 10);
 
   line = "Hello " + name + ", as of " + M + "/" + D + "/" + Y + " you are " + age + " years old.";
 
   if (user === targ) {
     hScore++;
   }
+  else {
+    hScore+= 0;
+  }
 
   if (comp === targ) {
     cScore++;
+  }
+  else {
+    cScore+= 0;
   }
 
   lineB = "Your score is " + hScore + " and the computer's score is " + cScore + ".";
