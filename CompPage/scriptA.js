cee4d28176c316f3515052289867f51889b30301
Mat <mjadams421@gmail.com>
2026-09-01 23:24:12 +0000

    Added to JS

---
+++ CompPage/scriptA.js
@@ -0,0 +1,24 @@
+
+const actionClick = document.getElementById("actionClick")
+const answer = document.getElementById("answer");
+
+values = [];
+let response = "";
+
+//function for adding numbers in a sentence
+function numbersAdd() {
+ let Value1 = window.prompt("Enter your house number");
+ let Value2 = window.prompt("Enter your age");
+ let Value3 = window.prompt("Enter years of higher education");
+ values.push(Value1, Value2, Value3);
+
+ let random = Math.floor(Math.random()*100)+1;
+ let addedValues = values.map( x => {return x*random});
+ for(let y of values){
+    response += y;
+ }
+  answer.innerHTML = 'Added values at random are: ' + response;
+
+}
+
+actionClick.addEventListener("mousedown", numbersAdd);
